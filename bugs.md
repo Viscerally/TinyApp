24b54a6b1c69223df41e83e94e773b9c64963335
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-06-14 02:17:24 +0000

    added node-modules to gitignore, fixed some other bugs and generally cleaned up most of the code

---
+++ bugs.md
@@ -0,0 +1,19 @@
+## Bugs:
+```
+- [x] Be sure not to commit node_modules folder.
+Add this path to a .gitignore file;
+commit that file and remove this folder. 
+- [x] on login attempt of a correct email/pw, I get: TypeError: Cannot set property 'user_id' of undefined. This is a major feature that doesn't work.
+- [x] on register, seems to be logged in, but /urls doesn't have a link for creating a new shorturl.
+- [x] on hitting /, if logged in, should redirect to /urls.
+- [x] on accessing /login or /register while logged in, should redirect to /urls. currently displays that page.
+```
+
+- [x]accessing something like 
+http://localhost:8080/u/086mL doesn't work.
+```
+ this should redirect to the url entered. It looks like the code for that is completely commented out. This is the main feature and should be functional before submitting.
+
+- [ ] Add in morgan npm to log request data
+
+```
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser"); // pulls the body parser middleware library
 const cookieSession = require('cookie-session')
 const bcrypt = require('bcrypt');
-// const password = ""; // you will probably this from req.params
+const morgan = require('morgan');
+
+
+
 
 
 var app = express();
@@ -12,7 +15,7 @@ var PORT = process.env.PORT || 8080; // default port 8080
 app.use(cookieSession({
   name: 'session',
   keys: ['key'],
-
+  
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
@@ -20,16 +23,33 @@ app.use(cookieSession({
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true })); // wires up Body Parser middleware
 
+app.use(morgan('combined')); //wires up morgan to log req details
 
 // databases
 var urlDatabase = {
-  "userRandomID": {
-    "b2xVn2": "http://www.lighthouselabs.ca"
-  },
-  "user2RandomID": {
-    "9sm5xK": "http://www.google.com"
-  }
+  // "userRandomID": {
+  //   "b2xVn2": "http://www.lighthouselabs.ca"
+  // },
+  // "user2RandomID": {
+  //   "9sm5xK": "http://www.google.com"
+  // }
 };
+
+// var tempDatabase = {
+//   "b2xVn2": {
+//     "longUrl": "http://www.lighthouselabs.ca",
+//     "user_id": "userRandomID"
+//   },
+//   "9sm5xK": {
+//     "longUrl": "http://www.google.com",
+//     "user_id": "userRandomID"
+//   },
+//   "4353ccc": {
+//     "longUrl": "http://www.facebook.com",
+//     "user_id": "userID3"
+//   }
+
+// };
 
 const users = {
   "userRandomID": {
@@ -48,7 +68,7 @@ const users = {
 function generateRandomString() {
   var newStr = "";
   var stringOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  
+
   for (var i = 0; i < 5; i++) {
     newStr += stringOptions.charAt(Math.floor(Math.random() * stringOptions.length));
   }
@@ -59,7 +79,7 @@ function lookUpUserObject(userId) {
   return users[userId]
 }
 
-// de facto "home page"; 
+//home page; 
 app.get("/", (req, res) => {
   res.render("urls_home");
 });
@@ -71,7 +91,6 @@ app.get("/", (req, res) => {
 
 app.get("/urls", (req, res) => {
   let userObject = lookUpUserObject(req.session.user_id);
-  // console.log("we are in /urls");
   if (userObject) {
     var urls;
     var id = userObject.id;
@@ -106,32 +125,31 @@ app.get("/urls/new", (req, res) => {
     };
     // let userObject = lookUpUserObject(req.cookies.user_id);
     // let templateVars = {
-      // user: userObject};
-      res.render("urls_new", templateVars);
-    } else {
-      res.redirect("/urls/register");
-    };
-  });
-  
-  app.post("/urls/:id", (req, res) => {
-    let userObject = lookUpUserObject(req.session.user_id);
-    if (userObject) {
-      var urls;
-      var id = userObject.id;
-      if (urlDatabase[id]) {
-        urls = urlDatabase[id];
-      }
-  var shortUrl = req.params.id;
-  var longUrl = req.body.longURL;
-  urlDatabase[id][shortUrl] = longUrl;
-  res.redirect("/urls");
+    // user: userObject};
+    res.render("urls_new", templateVars);
+  } else {
+    res.redirect("/urls/register");
+  };
+});
+
+app.post("/urls/:id", (req, res) => {
+  let userObject = lookUpUserObject(req.session.user_id);
+  if (userObject) {
+    var urls;
+    var id = userObject.id;
+    if (urlDatabase[id]) {
+      urls = urlDatabase[id];
+    }
+    var shortUrl = req.params.id;
+    var longUrl = req.body.longURL;
+    urlDatabase[id][shortUrl] = longUrl;
+    res.redirect("/urls");
   } else {
     res.redirect("/urls/register");
   };
 });
 
 app.get("/urls/:id", (req, res) => {
-  // console.log("we came from urls/id")
   let userObject = lookUpUserObject(req.session.user_id);
   if (userObject) {
     var urls;
@@ -157,7 +175,7 @@ app.get("/urls/:id", (req, res) => {
 ////////deletes urls
 app.post("/urls/:id/delete", (req, res) => {
   let userObject = lookUpUserObject(req.session.user_id);
-  
+
   if (userObject) {
     var id = userObject.id;
     let short = req.params.id
@@ -171,13 +189,13 @@ app.post("/urls/:id/delete", (req, res) => {
 // adds url to 'database object'
 app.post("/urls", (req, res) => {
   let userObject = lookUpUserObject(req.session.user_id);
-  
+
   if (userObject) {
     let shortUrl = generateRandomString();
     let longUrl = req.body.longURL;
     urlDatabase[userObject.id] = {};
     urlDatabase[userObject.id][shortUrl] = longUrl;
-    // console.log(urlDatabase);
+
     res.redirect("/urls");
   } else {
     res.redirect("/urls/register");
@@ -190,20 +208,24 @@ app.post("/urls", (req, res) => {
 // routes for login/register/logout
 
 app.get("/login", (req, res) => {
-  res.render("urls_login")
+  if (users[req.session.user_id]) {
+    res.redirect("/urls");
+  } else {
+    res.render("urls_login")
+  }
 });
 
 app.post("/login", (req, res) => {
   // bcrypt.compareSync(password, hashedPassword);
   let username = req.body.username;
   let password = req.body.password;
-  
+
   //find in users object where email is === username
   let foundUser = null;
   // console.log('USERS', users)
   for (const user in users) {
-    if (users[user].email === username) { 
-      if (bcrypt.compareSync(password, users[user].password)){
+    if (users[user].email === username) {
+      if (bcrypt.compareSync(password, users[user].password)) {
         foundUser = users[user]
       }
     }
@@ -211,10 +233,10 @@ app.post("/login", (req, res) => {
   if (foundUser === null) {
     res.status(403).send("No matching email")
   } else {
-    res.session.user_id = "user_id", foundUser.id;
+    req.session.user_id = foundUser.id;
     res.redirect("/urls");
   }
-  
+
 });
 
 app.post("/logout", (req, res) => {
@@ -223,7 +245,7 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  let username = req.body.username;
+  // let username = req.body.username;
   res.render("urls_register");
 });
 
@@ -231,30 +253,41 @@ app.post("/register", (req, res) => {
   let password = req.body.password;
   const hashedPassword = bcrypt.hashSync(password, 10); //turns password into hashed password
   // bcrypt.compareSync("123", hashedPassword);
-  
-  let userId = generateRandomString();
-  let username = req.body.username;
+
+  var userId = generateRandomString();
+  var username = req.body.username;
   if (username === "" || password === "") {
     res.status(400).send("ERROR: 400 \n You didn't enter a username or password you goomba")
   }
-  let userObject = {
+  var userObject = {
     id: userId,
     email: username,
     password: hashedPassword
   };
   users[userId] = userObject;
-    // console.log(users);
+
+  // console.log(users);
   // res.cookie("user_id", userId);
   req.session.user_id = userId;
   res.redirect("/urls");
 });
 
-// app.get("/u/:shortURL", (req, res) => {
-//   let key = req.params.shortURL;
-//   // console.log(urlDatabase[key]);
-//   let longURL = urlDatabase[][];
-//   res.redirect(longURL);
-// });
+app.get("/u/:shortURL", (req, res) => {
+  var shortUrl = req.params.shortURL;
+  var longUrl;
+  for (ID in urlDatabase) {
+    longUrl = urlDatabase[ID][shortUrl];
+
+    if (longUrl) {
+      break;
+    }
+  }
+  if (longUrl) {
+    res.redirect("http://" + longUrl); //this will redirect to "http://localhost:8080/u/www.bikes.com" which is a relative domain 
+  }
+  console.log("this is the longUrl", urlDatabase); 
+  res.status(404).send("Page not found");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
